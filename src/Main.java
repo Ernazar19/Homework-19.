60fc258a0017ab550275895aa6d2e85ba67b9d81
ernazaresanbekov <ernazar.03@icloud.com>
2023-02-21 17:23:25 +0600

    Home Work-19

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Planet planet = Planet.EARTH;
-        System.out.println(planet.getPlanet());
-
+        Planet[] planet = Planet.values();
+        while (true) {
+            String a = new Scanner(System.in).nextLine().toUpperCase();
+            switch (a) {
+                case "EARTH" -> System.out.println(Planet.EARTH.getPlanet());
+                case "MARS" -> System.out.println(Planet.MARS.getPlanet());
+                case "SATURN" -> System.out.println(Planet.SATURN.getPlanet());
+                case "MERCURY" -> System.out.println(Planet.MERCURY.getPlanet());
+                case "URANUS" -> System.out.println(Planet.URANUS.getPlanet());
+                case "JUPITER" -> System.out.println(Planet.JUPITER.getPlanet());
+                case "VENUS" -> System.out.println(Planet.VENUS.getPlanet());
+                default -> System.out.println("Мындай планета тузулгон жок");
+            }
+        }
     }
 }
