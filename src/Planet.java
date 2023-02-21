2466f930a84075ee639dbfd1369cc1e204514ece
ernazaresanbekov <ernazar.03@icloud.com>
2023-02-21 16:02:11 +0600

    HomeWork-19

--- src/Planet.java
+++ src/Planet.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Planet {
+public enum Planet {
+    EARTH("Жер планетасында жан бар жана орточо температурасы 14,8С"),
+    MARS("Марс жер планетасына окшош"),
+    SATURN("Сатурн Кундон алыстыгы боюнча 6 оорунда турат кун" +
+            " системасында чондугу боюнча Юпитерден кийинки экинчи планета ");
+    private  String planet;
+
+    Planet(String planet) {
+        this.planet = planet;
+    }
+
+    public String getPlanet() {
+        return planet;
+    }
+
+    public void setPlanet(String planet) {
+        this.planet = planet;
+    }
 }
